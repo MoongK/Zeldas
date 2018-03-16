2c1cf0644cd07fef874b5621a6083c5c0ca3a616
MoongK <qorfyd01@gmail.com>
2018-03-16 16:06:31 +0900

    Must be complete in Weekend

---
+++ Assets/HeartChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartChanger : MonoBehaviour {
+
+    Image myIm;
+
+	void Start () {
+        myIm = GetComponent<Image>();
+        StartCoroutine(filling());
+        myIm.fillAmount = 1f;
+	}
+	
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+            StartCoroutine(filling());
+        else if (Input.GetMouseButtonDown(1))
+            StartCoroutine(unfilling());
+	}
+
+    IEnumerator filling()
+    {
+        while (myIm.fillAmount > 0f)
+            myIm.fillAmount = Mathf.Lerp(myIm.fillAmount, 0f, 0.1f);
+
+        yield return null;
+    }
+    IEnumerator unfilling()
+    {
+        while(myIm.fillAmount <1f)
+            myIm.fillAmount = Mathf.Lerp(myIm.fillAmount, 1f, 0.1f);
+
+        yield return null;
+    }
+}
