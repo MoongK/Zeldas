a2ecbffeff80800d55f891d295069e43c9f2b9c3
MoongK <dijiket@naver.com>
2018-03-19 02:24:52 +0900

    second

--- Assets/HeartChanger.cs
+++ Assets/HeartChanger.cs
@@ -6,33 +6,44 @@ using UnityEngine.UI;
 public class HeartChanger : MonoBehaviour {
 
     Image myIm;
+    float per = 0.1f;
+    float dist;
 
 	void Start () {
         myIm = GetComponent<Image>();
-        StartCoroutine(filling());
-        myIm.fillAmount = 1f;
-	}
-	
-	void Update () {
+        myIm.fillAmount = 0f;
 
+    }
+
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
             StartCoroutine(filling());
+
         else if (Input.GetMouseButtonDown(1))
             StartCoroutine(unfilling());
-	}
+    }
 
     IEnumerator filling()
     {
-        while (myIm.fillAmount > 0f)
-            myIm.fillAmount = Mathf.Lerp(myIm.fillAmount, 0f, 0.1f);
+        dist = 1 - per;
+        while (myIm.fillAmount < 1f)
+        {
+            myIm.fillAmount = Mathf.Lerp(myIm.fillAmount, 1f, 1 / dist);
+            dist = 0.9f * dist;
+            yield return new WaitForSeconds(1f);
+        }
 
-        yield return null;
     }
+
     IEnumerator unfilling()
     {
-        while(myIm.fillAmount <1f)
-            myIm.fillAmount = Mathf.Lerp(myIm.fillAmount, 1f, 0.1f);
-
-        yield return null;
+        dist = 1 - per;
+        while (myIm.fillAmount > 0f)
+        {
+            myIm.fillAmount = Mathf.Lerp(myIm.fillAmount, 0f, 1 / dist);
+            dist = 0.9f * dist;
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
